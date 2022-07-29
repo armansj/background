48ba8c029ad505dd154583b476246c6a6abb0ad2
Armansj <armansajedi944@gmail.com>
2022-07-29 12:38:45 +0200

    Bug Fixed

---
+++ build/precache-manifest.b63276c3ce5866948b65765c7bb8384d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c798239b9eaae0e5dd6",
+    "url": "/react1/static/css/main.665c91f0.chunk.css"
+  },
+  {
+    "revision": "7c798239b9eaae0e5dd6",
+    "url": "/react1/static/js/main.7c798239.chunk.js"
+  },
+  {
+    "revision": "c41c1c1a55ea0027eb7b",
+    "url": "/react1/static/js/1.c41c1c1a.chunk.js"
+  },
+  {
+    "revision": "d3fa16be499d3c4dd482",
+    "url": "/react1/static/css/2.908b4dc0.chunk.css"
+  },
+  {
+    "revision": "d3fa16be499d3c4dd482",
+    "url": "/react1/static/js/2.d3fa16be.chunk.js"
+  },
+  {
+    "revision": "70d9ec1f441b79b6b46f",
+    "url": "/react1/static/js/runtime~main.70d9ec1f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react1/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ea86fb203c4a85ddb29310cef7442b0d",
+    "url": "/react1/index.html"
+  }
+];
---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots =[
+{
+    id:1,
+    name:'Leanna Graham',
+    username:'Bret',
+    email:'sincere@april.biz'
+},
+
+{
+    id:2,
+    name:'arman sajed',
+    username:'arman',
+    email:'armansajedi@arm.biz'
+},
+{
+    id:3,
+    name:'kosar hasani',
+    username:'kosar',
+    email:'kosarhasani@gmail.com'
+},
+{
+    id:4,
+    name:'pooya poori',
+    username:'pooya',
+    email:'pooyapoori@gmail.com'
+},
+{
+    id:5,
+    name:'saba sabayi',
+    username:'saba',
+    email:'sabasabayi@gmail.com'
+},
+{
+    id:6,
+    name:'tala talayi',
+    username:'tala',
+    email:'talatalayi@gmail.com'
+},
+{
+    id:7,
+    name:'amir amiri',
+    username:'amir',
+    email:'amiramiry@gmail.com'
+},
+{
+    id:8,
+    name:'sajjad sajjadi',
+    username:'sajjad',
+    email:'sajjadsajjadi@gmail.com'
+},
+{
+    id:9,
+    name:'bahar bahari',
+    username:'bahar',
+    email:'baharbahari@gmail.com'
+},
+{
+    id:10,
+    name:'fateme havadar',
+    username:'fateme',
+    email:'fatemehavadar@gmail.com'
+},
+
+
+];
